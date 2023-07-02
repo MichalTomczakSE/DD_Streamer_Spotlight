146ed5ef2cce73173f388fab8306686e620947e3
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-07-02 23:52:46 +0200

    Delete unused controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {ormConfig} from "../ormconfig";
@@ -11,7 +10,7 @@ import { GatewayModule } from './gateway/gateway.module';
         TypeOrmModule.forRoot(ormConfig),
         StreamersModule,
         GatewayModule],
-    controllers: [AppController],
+    controllers: [],
     providers: [AppService],
 })
 export class AppModule {
