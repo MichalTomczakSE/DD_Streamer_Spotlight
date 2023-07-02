0785d6a283bae119bcbcc8ad5d1aecd1c9a10349
Michał Tomczak <116025688+MichalTomczakSE@users.noreply.github.com>
2023-07-02 16:09:00 +0200

    Revert "PR #3 Added methods to streamers service"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {ormConfig} from "../ormconfig";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig)],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/streamers/streamers.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StreamersService } from './streamers.service';
-import { StreamersController } from './streamers.controller';
-
-@Module({
-  controllers: [StreamersController],
-  providers: [StreamersService]
-})
-export class StreamersModule {}
