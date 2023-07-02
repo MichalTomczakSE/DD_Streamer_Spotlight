3e62ccf6b1a73ccc467831b5ba9e2155331d2f59
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-07-02 14:00:53 +0200

    Created streamer entity

--- src/streamers/entities/streamer.entity.ts
+++ src/streamers/entities/streamer.entity.ts
@@ -1,1 +1,48 @@
-export class Streamer {}
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity()
+export class Streamer {
+
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column({
+        unique: true,
+        length: 100,
+        nullable: false
+    })
+    username: string;
+
+
+    @Column({
+        length: 7,
+        nullable: false
+    })
+    platform: string;
+
+
+    @Column({
+        length: 2000,
+        nullable: true
+    })
+    description: string | null;
+
+    @Column({
+        default: 0,
+        type: "integer"
+    })
+    upVotes: number;
+
+
+    @Column({
+        default: 0,
+        type: "integer"
+    })
+    downVotes: number;
+
+    @Column({
+        default: null,
+        nullable: true
+    })
+    imageFn: string;
+}
