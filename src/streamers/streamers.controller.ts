0acc21a0ec91e767872f15fc1ebcfeb18c066944
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-07-02 13:58:23 +0200

    Create streamers resource with nest generate

---
+++ src/streamers/streamers.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { StreamersService } from './streamers.service';
+import { CreateStreamerDto } from './dto/create-streamer.dto';
+import { UpdateStreamerDto } from './dto/update-streamer.dto';
+
+@Controller('streamers')
+export class StreamersController {
+  constructor(private readonly streamersService: StreamersService) {}
+
+  @Post()
+  create(@Body() createStreamerDto: CreateStreamerDto) {
+    return this.streamersService.create(createStreamerDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.streamersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.streamersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateStreamerDto: UpdateStreamerDto) {
+    return this.streamersService.update(+id, updateStreamerDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.streamersService.remove(+id);
+  }
+}
