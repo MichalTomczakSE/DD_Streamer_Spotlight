5c96c89bdf30b3cfaaf150ba2e93ba78d5f08afa
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-07-02 16:29:31 +0200

    Configure websocket gateway

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -1,4 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { VotesGateway } from "./votes.gateway";
+import { Streamer } from "../streamers/entities/streamer.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { StreamersService } from "../streamers/streamers.service";
 
-@Module({})
-export class GatewayModule {}
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Streamer])],
+    providers: [VotesGateway, StreamersService]
+})
+export class GatewayModule {
+
+}
