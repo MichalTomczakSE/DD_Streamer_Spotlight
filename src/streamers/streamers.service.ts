fe8160ae41acc761cd6f57db7591add876e0970c
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-07-02 14:47:26 +0200

    Added create method for adding new Streamer to database

--- src/streamers/streamers.service.ts
+++ src/streamers/streamers.service.ts
@@ -1,18 +1,30 @@
-import {Injectable} from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
 import {CreateStreamerDto} from './dto/create-streamer.dto';
 import {UpdateStreamerDto} from './dto/update-streamer.dto';
 import {InjectRepository} from "@nestjs/typeorm";
 import {Streamer} from "./entities/streamer.entity";
 import {Repository} from "typeorm";
+import {GetStreamerData, GetStreamersData} from "../types";
 
 @Injectable()
 export class StreamersService {
     constructor(
         @InjectRepository(Streamer) private streamersRepository: Repository<Streamer>
-    ) {}
+    ) {
+    }
 
-    create(createStreamerDto: CreateStreamerDto) {
-        return 'This action adds a new streamer';
+    async create(req: CreateStreamerDto): Promise<GetStreamersData | BadRequestException> {
+        const existingStreamer = await this.streamersRepository.findOneBy({username: req.username})
+        if (existingStreamer) {
+            throw new BadRequestException({
+                message: `${existingStreamer.username} is already created on site`,
+                existingStreamer: existingStreamer.id
+            });
+        }
+        const streamer = this.streamersRepository.create(req);
+        await this.streamersRepository.save(streamer);
+        const {imageFn, description, ...streamerData} = streamer;
+        return streamerData;
     }
 
     findAll() {
