980d79c75ef28fef61c692547afc6afb89b03f22
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-07-02 14:32:10 +0200

    Added pipes to controller, changed return types

--- src/streamers/streamers.controller.ts
+++ src/streamers/streamers.controller.ts
@@ -1,7 +1,19 @@
-import {Controller, Get, Post, Body, Param, Put, Res} from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Param,
+    Put,
+    Res,
+    ParseUUIDPipe,
+    BadRequestException,
+    NotFoundException
+} from '@nestjs/common';
 import {StreamersService} from './streamers.service';
 import {CreateStreamerDto} from './dto/create-streamer.dto';
 import {UpdateStreamerDto} from './dto/update-streamer.dto';
+import {GetStreamerData, GetStreamersData, UpdatedStreamerData} from "../types";
 
 @Controller('streamers')
 export class StreamersController {
@@ -9,27 +21,27 @@ export class StreamersController {
     }
 
     @Post()
-    create(@Body() createStreamerDto: CreateStreamerDto) {
-        return this.streamersService.create(createStreamerDto);
+    create(@Body() req: CreateStreamerDto): Promise<GetStreamerData | BadRequestException> {
+        return;
     }
 
     @Get()
-    findAll() {
-        return this.streamersService.findAll();
+    findAll(): Promise<GetStreamersData[] | NotFoundException> {
+        return;
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.streamersService.findOne(+id);
+    findOne(@Param('id', ParseUUIDPipe) id: string): Promise<GetStreamerData | NotFoundException> {
+        return;
     }
 
     @Put(':id/vote')
-    update(@Param('id') id: string, @Body() updateStreamerDto: UpdateStreamerDto) {
-        return this.streamersService.update(+id, updateStreamerDto);
+    update(@Param('id', ParseUUIDPipe) id: string, @Body() req: UpdateStreamerDto): Promise<UpdatedStreamerData | BadRequestException> {
+        return;
     }
 
     @Get("/image/:id")
-    async getImage(@Param('id') id: string,
+    async getImage(@Param('id', ParseUUIDPipe) id: string,
                    @Res() res: any): Promise<any> {
         return this.streamersService.getImage(id, res)
     }
